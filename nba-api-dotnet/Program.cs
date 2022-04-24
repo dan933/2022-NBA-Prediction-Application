866ef397449b5de6a4183286b62065b2341a2025
dan933 <69749413+dan933@users.noreply.github.com>
2022-04-24 19:49:43 +1000

    commented out github actions file

--- nba-api-dotnet/Program.cs
+++ nba-api-dotnet/Program.cs
@@ -54,7 +54,7 @@ builder.Services.AddSwaggerGen(options =>
 
 builder.Services.AddDbContext<NBAContext>(options =>
 {
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DanLaptopDB"));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AzureDatabase"));
 
 });
 
