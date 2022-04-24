cb409cc67fe1b6e0f22869aad1c348c61e110c5a
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-04-25 01:33:41 +1000

    Added Title and Create Team Button

--- nba-front-end/src/components/teamDataGrid/teamDataGridPage.tsx
+++ nba-front-end/src/components/teamDataGrid/teamDataGridPage.tsx
@@ -3,33 +3,33 @@ import { Button } from '@mui/material';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
 import { makeStyles } from '@material-ui/core/styles';
 // import FilledTeamsTable from './teamsTableLoader';
 import FilledTeamTable from './teamTableLoader';
 
-<Button 
-  variant="contained" 
-  color="primary"
-  style={{
-    top:100,
-    left:200,
-    bottom:500,
-    right:200,
-    width:100,
-    height:40,
-    position: 'absolute',}}>
-Create Team
-</Button>
 
 function teamPage() {
   return (
     <Container maxWidth="lg" sx={{ mt: 4, mb: 4, "min-height":600, }}>
+      <Button variant="contained" color="primary" size="large"
+      style={{
+        margin:0,
+        position: 'relative',
+        top:10,
+        left:-330,}}>
+          Create Team
+      </Button>
+
+      <h1 style={{margin:0, width:5, position: 'relative', top:-33, left:-170}}>Teams</h1>
+
         <Grid container spacing={3}>
             {/* Chart */}
             <Grid item xs={10} md={8} lg={8}>
             {/* {<FilledTeamsTable></FilledTeamsTable>} */}
             {<FilledTeamTable></FilledTeamTable>}
-            
             </Grid>
         </Grid>
     </Container>
