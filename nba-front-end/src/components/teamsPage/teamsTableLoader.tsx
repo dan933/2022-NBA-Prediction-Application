5355b63fc0b3320061dab7745d0750de17ede169
Hussein Hussein <103615588@student.swin.edu.au>
2022-04-27 00:42:02 +1000

    added local host in api string

--- nba-front-end/src/components/teamsPage/teamsTableLoader.tsx
+++ nba-front-end/src/components/teamsPage/teamsTableLoader.tsx
@@ -1,36 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import DataGridTeams from './teamsDataGrid';
-import WithTableLoading from '../componentLoading';
-import api from '../../services/api';
-import { Teams } from '../../models/ITeams';
+import React, { useEffect, useState } from "react";
+import DataGridTeams from "./teamsDataGrid";
+import WithTableLoading from "../componentLoading";
+import api from "../../services/api";
+import { Teams } from "../../models/ITeams";
 
-
-interface teamsProps{
-    loading: boolean;
-    teamsList: Teams[];
+interface teamsProps {
+  loading: boolean;
+  teamsList: Teams[];
 }
 
 function FilledTeamsTable() {
-    const TableLoading = WithTableLoading(DataGridTeams);
-    const [appState, setAppState] = useState<teamsProps>({
-        loading: false,
-        teamsList: [],
-      });
+  const TableLoading = WithTableLoading(DataGridTeams);
+  const [appState, setAppState] = useState<teamsProps>({
+    loading: false,
+    teamsList: [],
+  });
 
-      useEffect(() => {
-        setAppState({ loading: true, teamsList: [] });
-        api.get('players/get-all').subscribe((resp) => {
-            setAppState({ loading: false, teamsList: resp as Teams[] });
-          });
-      }, [setAppState]);
-    
-      
+  useEffect(() => {
+    setAppState({ loading: true, teamsList: [] });
+    api.get("players/get-all").subscribe((resp) => {
+      setAppState({ loading: false, teamsList: resp as Teams[] });
+    });
+  }, [setAppState]);
+
   return (
     <div>
-        <TableLoading isLoading={appState.loading} teamsList={appState.teamsList} />
+      <TableLoading
+        isLoading={appState.loading}
+        teamsList={appState.teamsList}
+      />
     </div>
-);
-
+  );
 }
 
 export default FilledTeamsTable;
