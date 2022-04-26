83d5c9c894ab5658b2c3c5fa76c6fd8be6617c67
Hussein Hussein <103615588@student.swin.edu.au>
2022-04-27 00:45:28 +1000

    Formatted teams page: - New Add Team Button (with popup) - New dialogue screen for view teams ( on cell click ) - Small Formatting fixes - Inclusion of 2nd table for player selection - Minor code refactoring for teamsData table - Comments added

--- nba-front-end/src/components/teamsPage/teamsDataGrid.tsx
+++ nba-front-end/src/components/teamsPage/teamsDataGrid.tsx
@@ -1,67 +1,165 @@
-import * as React from 'react';
-import { DataGrid, GridColDef, GridFilterModel, GridValueGetterParams } from '@mui/x-data-grid';
-import { FormControl, Grid, IconButton, Input, InputAdornment, InputLabel, OutlinedInput, Paper, TextField } from '@mui/material';
-import SearchIcon from '@mui/icons-material/Search';
-import { useEffect } from 'react';
-
+import React, { useState } from "react";
+import {
+  DataGrid,
+  GridColDef,
+  GridFilterModel,
+  GridValueGetterParams,
+} from "@mui/x-data-grid";
+import {
+  FormControl,
+  Grid,
+  IconButton,
+  Input,
+  InputAdornment,
+  InputLabel,
+  OutlinedInput,
+  Paper,
+  TextField,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogContentText,
+  DialogTitle,
+} from "@mui/material";
+import SearchIcon from "@mui/icons-material/Search";
+import { useEffect } from "react";
+import Button from "@mui/material/Button";
+import AddIcon from "@mui/icons-material/Add";
+import FilledPlayerTable from "../playerDataGrid/playerTableLoader";
 
 const teamsColumns: GridColDef[] = [
-    { field: 'TeamID', headerName: 'ID', width: 90, hide: false },
-    { field: 'TeamName', headerName: 'Team Name', width: 150, },
-    { field: 'TeamWinPercentage', headerName: 'Win Percentage', width: 150,
-      valueFormatter: (params) => {
-        const valueFormatted = Number((params.value as number) * 100).toLocaleString();
-        return `${valueFormatted} %`; 
+  { field: "TeamID", headerName: "ID", width: 90, hide: false },
+  { field: "TeamName", headerName: "Team Name", width: 150 },
+  {
+    field: "TeamWinPercentage",
+    headerName: "Win Percentage",
+    width: 150,
+    valueFormatter: (params) => {
+      const valueFormatted = Number(
+        (params.value as number) * 100
+      ).toLocaleString();
+      return `${valueFormatted} %`;
     },
-},
+  },
+];
 
-  ];
+// todo connect API instead of hard coding
+const rows = [
+  { TeamID: 1, TeamName: "Team 1", TeamWinPercentage: 0.43 },
+  { TeamID: 2, TeamName: "Team 2", TeamWinPercentage: 0.25 },
+  { TeamID: 3, TeamName: "Team 3", TeamWinPercentage: 0.1 },
+];
 
-  const rows = [
-    { TeamID: 1, TeamName: 'Team 1', TeamWinPercentage: 0.43},
-    { TeamID: 2, TeamName: 'Team 2', TeamWinPercentage: 0.25},
-    { TeamID: 3, TeamName: 'Team 3', TeamWinPercentage: 0.10 },
-   
-  ];
-
-  
 const DataGridTeams: React.FC<any> = (props) => {
-    
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const [view, viewAll] = React.useState(false);
+  const teamInfo = () => {
+    viewAll(true);
+  };
+
+  const closeTeamView = () => {
+    viewAll(false);
+  };
+
+  //todo: onclick create new team entry in table
+  const updateTeams = () => {
+    //code here
+    viewAll(true);
+  };
   return (
     // white box around the table
-    <Paper 
-        sx={{
-          p: 2,
-          paddingTop:5,
-          display: 'flex',
-          flexDirection: 'column',
-          height: 'auto',
-          mt: 'auto',
-          position:'relative', 
-          left:'-165px',
-          width:'482px'
-        }}
-      >
-        {/* formats the placement of the searchbar and table */}
-        <Grid container spacing={3}/>
-         <Grid item xl={3} md={6} xs={12}/>
-          <Grid item xs={12}>
-            <div style={{ height: '800px', width: '450px', position:'relative'}}>
-              <DataGrid
-              rows={rows}
-              getRowId={(row) => row.TeamID}
-              columns={teamsColumns}
-              disableColumnSelector={true}
-              pageSize={10}
-              rowsPerPageOptions={[5]}
-              checkboxSelection
-              disableSelectionOnClick
+    <Paper
+      sx={{
+        p: 2,
+        paddingTop: 5,
+        display: "flex",
+        flexDirection: "column",
+        height: "auto",
+        mt: "auto",
+        position: "relative",
+        // distance from the left side of the page
+        left: "-382px",
+        width: "482px",
+      }}
+    >
+      {/* formats the placement of the searchbar and table */}
+      <Grid container spacing={3} />
+      <Grid item xl={3} md={6} xs={12} />
+      <Grid item xs={12}>
+        <div style={{ height: "800px", width: "452px", position: "relative" }}>
+          <DataGrid
+            onRowClick={teamInfo}
+            rows={rows}
+            getRowId={(row) => row.TeamID}
+            columns={teamsColumns}
+            disableColumnSelector={true}
+            pageSize={10}
+            rowsPerPageOptions={[5]}
+            disableSelectionOnClick
+          />
+          {/* displays the pop up dialogue box on row click */}
+          <Dialog id="viewTeam" open={view} onClose={handleClose}>
+            <DialogTitle>Team (test) </DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                View All Players In The Team
+                <FilledPlayerTable></FilledPlayerTable>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={closeTeamView}>Cancel</Button>
+              <Button onClick={updateTeams}>Save Edits</Button>
+            </DialogActions>
+          </Dialog>
+          {/* add team button */}
+          <Button
+            sx={{
+              left: "-148px",
+              top: "-45px",
+              padding: "5px",
+              paddingRight: "9px",
+            }}
+            variant="contained"
+            color="success"
+            startIcon={<AddIcon />}
+            onClick={handleClickOpen}
+          >
+            Add New Team
+          </Button>
+          {/* secondary dialogue box for add player */}
+          <Dialog id="createTeam" open={open} onClose={handleClose}>
+            <DialogTitle>Create a new team:</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                To create a new team, please provide a Team Name.
+              </DialogContentText>
+              <TextField
+                autoFocus
+                margin="dense"
+                id="TeamName"
+                label="Team Name"
+                type="Team Name"
+                fullWidth
+                variant="standard"
               />
-            </div>
-          </Grid>
-      </Paper>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={handleClose}>Cancel</Button>
+              <Button onClick={handleClose}>Create </Button>
+            </DialogActions>
+          </Dialog>
+        </div>
+      </Grid>
+    </Paper>
   );
-}
-
+};
 
 export default DataGridTeams;
--- nba-front-end/src/components/teamsPage/teamsPage.tsx
+++ nba-front-end/src/components/teamsPage/teamsPage.tsx
@@ -1,34 +1,36 @@
-import * as React from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
+//teams page
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
 // import FilledTeamsTable from './teamsTableLoader';
-import DataGridTeams from './teamsDataGrid';
+import DataGridTeams from "./teamsDataGrid";
+import FilledPlayerTable from "../playerDataGrid/playerTableLoader";
 
 function teamsPage() {
   return (
-    <Container maxWidth="lg" sx={{ mt: 4, mb: 4, "min-height":600, }}>
-        <Grid container spacing={3}>
-            {/* Chart */}
-            <Grid item xs={10} md={8} lg={8}>
-            {/* {<FilledTeamsTable></FilledTeamsTable>} */}
-            {<DataGridTeams></DataGridTeams>}
-            
-            </Grid>
-        </Grid>
-    </Container>
+    <div
+      style={{
+        paddingLeft: "10px",
+        display: "flex",
+        marginTop: "20px",
+        justifyContent: "center",
+        columnGap: "10px",
+      }}
+    >
+      <DataGridTeams></DataGridTeams>
+      <FilledPlayerTable></FilledPlayerTable>
+    </div>
   );
 }
 
 <Paper
-        sx={{
-          p: 2,
-          display: 'flex',
-          flexDirection: 'column',
-          height: 'auto',
-        }}
-  />
-  
-
+  sx={{
+    p: 2,
+    display: "flex",
+    flexDirection: "column",
+    height: "auto",
+  }}
+/>;
 
 export default teamsPage;
