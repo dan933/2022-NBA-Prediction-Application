bb4039c79bb36015f2118c5c497f6ad538d66ef9
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-04-27 20:56:04 +1000

    Added Error State for PlayerDataGridPage

--- nba-front-end/src/services/axios_config.ts
+++ nba-front-end/src/services/axios_config.ts
@@ -1,7 +1,7 @@
 import { AxiosRequestConfig } from 'axios';
 
 export const axiosRequestConfiguration: AxiosRequestConfig = {
-    baseURL: `${process.env.REACT_APP_AzureAPIDotNet}`,
+    baseURL: `${process.env.REACT_APP_MockAPI}`,
     responseType: 'json',
     headers: {
         'Content-Type': 'application/json',
