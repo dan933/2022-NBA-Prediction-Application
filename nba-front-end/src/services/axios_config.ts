d916e81aaf818cd457abca7cd6d4d898efddfa9e
dan933 <69749413+dan933@users.noreply.github.com>
2022-05-02 15:59:57 +1000

    Update axios_config.ts

--- nba-front-end/src/services/axios_config.ts
+++ nba-front-end/src/services/axios_config.ts
@@ -1,7 +1,7 @@
 import { AxiosRequestConfig } from 'axios';
 
 export const axiosRequestConfiguration: AxiosRequestConfig = {
-    baseURL: `${process.env.REACT_APP_LocalAPIDotNet}`,
+    baseURL: `${process.env.REACT_APP_AzureAPIDotNet}`,
     responseType: 'json',
     headers: {
         'Content-Type': 'application/json',
