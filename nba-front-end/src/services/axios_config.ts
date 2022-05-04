6909584db34c3d69de6f8863aa8bd6164b9ea149
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-05-04 20:01:47 +1000

    Delete axios_config.ts

--- nba-front-end/src/services/axios_config.ts
+++
@@ -1,9 +0,0 @@
-import { AxiosRequestConfig } from 'axios';
-
-export const axiosRequestConfiguration: AxiosRequestConfig = {
-    baseURL: `${process.env.REACT_APP_LocalAPIDotNet}`,
-    responseType: 'json',
-    headers: {
-        'Content-Type': 'application/json',
-    },
-};
