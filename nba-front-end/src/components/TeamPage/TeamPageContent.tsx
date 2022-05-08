5fc17fac261619193ef3dcbdddbd795841e8a77f
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-05-08 13:40:45 +1000

    Update TeamPageContent.tsx

--- nba-front-end/src/components/TeamPage/TeamPageContent.tsx
+++ nba-front-end/src/components/TeamPage/TeamPageContent.tsx
@@ -43,8 +43,6 @@ const TeamPageContent: React.FC<any> = (props) => {
 
   const [isUpdated, setIsUpdated] = React.useState(false);
 
-  const [newTeamID, setNewTeamID] = React.useState("");
-
   const tableIsUpdated = () => {
     setIsUpdated(true);
 
