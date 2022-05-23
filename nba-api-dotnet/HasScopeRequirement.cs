d7424b2d335492a0ad3bec718b4460c0d6f0d23a
EveJoyceStudent <80442618+EveJoyceStudent@users.noreply.github.com>
2022-05-23 16:39:52 +1000

    Added authorisation to all team API calls

---
+++ nba-api-dotnet/HasScopeRequirement.cs
@@ -0,0 +1,14 @@
+using System;
+using Microsoft.AspNetCore.Authorization;
+
+public class HasScopeRequirement : IAuthorizationRequirement
+{
+    public string Issuer { get; }
+    public string Scope { get; }
+
+    public HasScopeRequirement(string scope, string issuer)
+    {
+        Scope = scope ?? throw new ArgumentNullException(nameof(scope));
+        Issuer = issuer ?? throw new ArgumentNullException(nameof(issuer));
+    }
+}
