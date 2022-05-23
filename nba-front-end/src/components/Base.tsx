5568331bc058dbcbf4aed88dd684ebbca77fa047
Riki225 <79812982+Riki225@users.noreply.github.com>
2022-05-24 07:01:29 +1000

    Revised and Made Nav Bar Sticky

--- nba-front-end/src/components/Base.tsx
+++ nba-front-end/src/components/Base.tsx
@@ -1,18 +1,11 @@
 import * as React from 'react';
 import { Link, Outlet } from 'react-router-dom';
 import logo from '../logo.svg';
-import { ReactComponent as IconMenu } from '../images/top-nav-bar-img.svg'
 import { height } from '@mui/system';
 
 function BasePage() {
   return (
     <div>
-      <div className='Background-Color'>
-        <header className="App-header">
-  {/* ----------------------- nba image for top nav bar --------------- */}
-        <IconMenu/>
-        </header>
-      </div>
       {/* An <Outlet> renders whatever child route is currently active,
           so you can think about this <Outlet> as a placeholder for
         the child routes we defined above. */}        
